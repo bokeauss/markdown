f3554f205072405f41ea7ca9319d8d992e1d72e8
F.Javier Pedrosa <bokeauss@gmail.com>
2020-09-12 18:23:39 +0200

    Tutorial almost finished

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "styles/global.css"
+import "typeface-open-sans"
+import "typeface-merriweather"
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
